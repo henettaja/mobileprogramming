debdba7d7f1bf8107c0f5cb9035bf7a0fd668a4c
henettaja <henri.vaisane@gmail.com>
2020-09-12 14:05:53 +0300

    Added history to calculator

--- calculator/App.js
+++ calculator/App.js
@@ -1,19 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
 
   const [num1, setNum1] = React.useState("0");
   const [num2, setNum2] = React.useState("0");
   const [result, setResult] = React.useState(0);
+  const [data, setData] = React.useState([])
 
   const add = () => {
-    setResult(parseInt(num1)+parseInt(num2));
+    const res = parseInt(num1)+parseInt(num2)
+    setResult(res);
+
+    setData([{key: `${num1} + ${num2} = ${res}`}, ...data])
   }
 
   const subtract = () => {
-    setResult(parseInt(num1)-parseInt(num2));
+    const res = parseInt(num1)-parseInt(num2)
+    setResult(res);
+
+    setData([{key: `${num1} - ${num2} = ${res}`}, ...data])
   }
 
   return (
@@ -24,7 +31,12 @@ export default function App() {
         <Button onPress={add} title="+"/>
         <Button onPress={subtract} title="-"/>
       </View>
-      <Text>Result: {result} </Text>
+      <Text>Result: {result}</Text>
+      <Text>History: </Text>
+      <FlatList
+        data={data}
+        renderItem={({item}) =>
+            <Text>{item.key}</Text>}/>
       <StatusBar style="auto"/>
     </View>
   );
@@ -33,6 +45,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 100,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
